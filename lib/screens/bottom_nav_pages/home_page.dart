4f4d638e7172d8a1ff500bee5b9f09e6474f4306
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2021-04-01 12:23:28 +0600

    home screen edidted, homescreenchart adjusted

--- lib/screens/bottom_nav_pages/home_page.dart
+++ lib/screens/bottom_nav_pages/home_page.dart
@@ -1,140 +1,278 @@
 import 'package:fitness_app_x/widgets/home_page_widgets/home_page_chart.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? dropDownValue;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return SingleChildScrollView(
       child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Hey User!",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text("Wanna burn some calories today?",
-                style: Theme.of(context).textTheme.bodyText2),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 350,
-              height: 120,
-              decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(2, 2),
-                        color: Theme.of(context).primaryColor,
-                        blurRadius: 10)
-                  ],
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.blue),
-                  color: Theme.of(context).primaryColor),
-              child: CustomPaint(
-                // size: Size(300, 300),
-                painter: HomePageChart(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hey User!",
+                style: Theme.of(context).textTheme.headline6,
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Card(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Start Planning Meal",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepPurple),
-                    ),
-                    Text(
-                      "Balance your daily nutrition to stay healthy.",
-                      style: TextStyle(color: Colors.deepPurple),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          colors: [
-                            Theme.of(context).primaryColor,
-                            Theme.of(context).primaryColorLight,
-                          ],
+              Text("Wanna burn some calories today?",
+                  style: Theme.of(context).textTheme.bodyText2),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 280,
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(2, 2),
+                          color: Theme.of(context).primaryColor,
+                          blurRadius: 10)
+                    ],
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.blue),
+                    color: Theme.of(context).primaryColor),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Progress",
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: DropdownButton<String?>(
+                                value: dropDownValue,
+                                style: TextStyle(color: Colors.deepPurple),
+                                hint: Text("Select"),
+                                icon: Icon(Icons.keyboard_arrow_down,
+                                    color: Colors.deepPurple),
+                                items: <String?>[
+                                  "Running",
+                                  "Jumping",
+                                  "Joggling"
+                                ].map((e) {
+                                  return DropdownMenuItem<String?>(
+                                    value: e,
+                                    child: Text(e!),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    dropDownValue = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 140,
+                        child: Transform.scale(
+                          scale: 0.9,
+                          child: Padding(
+                            padding: EdgeInsets.all(12),
+                            child: CustomPaint(
+                              painter: HomePageChart(),
+                            ),
+                          ),
                         ),
                       ),
-                      width: 95,
-                      height: 31,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Sign In",
-                          style: TextStyle(color: Colors.white),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          RowItem(
+                            name: "Calories",
+                            number: "1.5",
+                            type: "k",
+                          ),
+                          RowItem(
+                            name: "Distance",
+                            number: "10",
+                            type: "km",
+                          ),
+                          RowItem(
+                            name: "Avg Time",
+                            number: "1h15",
+                            type: "m",
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Start Planning Meal",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepPurple),
+                      ),
+                      Text(
+                        "Balance your daily nutrition to stay healthy.",
+                        style: TextStyle(color: Colors.deepPurple),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: [
+                              Theme.of(context).primaryColor,
+                              Theme.of(context).primaryColorLight,
+                            ],
+                          ),
                         ),
+                        width: 95,
+                        height: 31,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Workouts",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text("Always stay in shape",
+                  style: Theme.of(context).textTheme.bodyText1),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                width: size.width - 50,
+                height: 300,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Card(
+                      child: Container(
+                        height: 150,
+                        width: 400,
+                        child: Center(child: Text("Workout List Item")),
+                      ),
+                      elevation: 10,
+                    ),
+                    Card(
+                      child: Container(
+                        height: 150,
+                        width: 400,
+                        child: Text("1"),
+                      ),
+                    ),
+                    Card(
+                      child: Container(
+                        height: 150,
+                        width: 400,
+                        child: Text("1"),
+                      ),
+                    ),
+                    Card(
+                      child: Container(
+                        height: 150,
+                        width: 400,
+                        child: Text("1"),
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Workouts",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Text("Always stay in shape",
-                style: Theme.of(context).textTheme.bodyText1),
-            SizedBox(
-              height: 5,
-            ),
-            Container(
-              width: size.width - 50,
-              height: 300,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Card(
-                    child: Container(
-                      height: 150,
-                      width: 400,
-                      child: Center(child: Text("Workout List Item")),
-                    ),
-                    elevation: 10,
-                  ),
-                  Card(
-                    child: Container(
-                      height: 150,
-                      width: 400,
-                      child: Text("1"),
-                    ),
-                  ),
-                  Card(
-                    child: Container(
-                      height: 150,
-                      width: 400,
-                      child: Text("1"),
-                    ),
-                  ),
-                  Card(
-                    child: Container(
-                      height: 150,
-                      width: 400,
-                      child: Text("1"),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class RowItem extends StatelessWidget {
+  final String? name;
+  final String? number;
+  final String? type;
+
+  RowItem({this.name, this.number, this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 80,
+      child: Column(
+        children: [
+          Text(
+            name!,
+            style: TextStyle(color: Color(0xffBDBDBD)),
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          RichText(
+            text: TextSpan(
+              text: number,
+              style: GoogleFonts.raleway(
+                  fontWeight: FontWeight.bold, fontSize: 30),
+              children: [
+                TextSpan(
+                  text: type,
+                  style: GoogleFonts.raleway(
+                      fontWeight: FontWeight.bold, fontSize: 15),
+                )
+              ],
+            ),
+            // "$number $type",
+            // style: TextStyle(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/home_page_widgets/home_page_chart.dart
+++ lib/widgets/home_page_widgets/home_page_chart.dart
@@ -1,47 +1,191 @@
 import 'package:flutter/material.dart';
 
 class HomePageChart extends CustomPainter {
-  // @override
-  // void paint(Canvas canvas, Size size) {
-  //   final p1 = Offset(0, 0);
-  //   final p2 = Offset(300, 0);
-  //   final p3 = Offset(0, 50);
-  //   final p4 = Offset(300, 50);
-  //   final p5 = Offset(0, 100);
-  //   final p6 = Offset(300, 100);
-  //   final p7 = Offset(0, 150);
-  //   final p8 = Offset(300, 150);
-  //   final paint = Paint()..color = Colors.white..strokeWidth = 0.5;
-  //   canvas.drawLine(p1, p2, paint);
-  //   canvas.drawLine(p3, p4, paint);
-  //   canvas.drawLine(p5, p6, paint);
-  //   canvas.drawLine(p7, p8, paint);
-  //   canvas.drawLine(p7, p8, paint);
-  //   final p9 = Offset(0, 200);
-  //   final p10 = Offset(50, 125);
-  //   final p11 = Offset(50, 125);
-  //   final p12 = Offset(100, 150);
-  //   final p13 = Offset(100, 150);
-  //   final p14 = Offset(150, 50);
-  //   final p15 = Offset(150, 50);
-  //   final p16 = Offset(200, 130);
-  //   final p17 = Offset(200, 130);
-  //   final p18 = Offset(250, 90);
-  //   final p19 = Offset(250, 90);
-  //   final p20 = Offset(300, 130);
-  //   final paint2 = Paint()..color = Colors.white..strokeWidth = 2;
-  //   canvas.drawLine(p9, p10, paint2);
-  //   canvas.drawLine(p11, p12, paint2);
-  //   canvas.drawLine(p13, p14, paint2);
-  //   canvas.drawLine(p15, p16, paint2);
-  //   canvas.drawLine(p17, p18, paint2);
-  //   canvas.drawLine(p19, p20, paint2);
-  // }
-
   @override
   void paint(Canvas canvas, Size size) {
+    final p1 = Offset(0, 0);
+    final p2 = Offset(300, 0);
+    final p3 = Offset(0, 30);
+    final p4 = Offset(300, 30);
+    final p5 = Offset(0, 60);
+    final p6 = Offset(300, 60);
+    final p7 = Offset(0, 90);
+    final p8 = Offset(300, 90);
+    final paintLines = Paint()
+      ..color = Colors.grey
+      ..strokeWidth = 0.5;
+    canvas.drawLine(p1, p2, paintLines);
+    canvas.drawLine(p3, p4, paintLines);
+    canvas.drawLine(p5, p6, paintLines);
+    canvas.drawLine(p7, p8, paintLines);
+    // Calories
+    final textStyle = TextStyle(
+      color: Color(0xffBDBDBD),
+      fontSize: 14,
+    );
+    final textSpan3 = TextSpan(
+      text: '3',
+      style: textStyle,
+    );
+    final textSpan2 = TextSpan(
+      text: '2',
+      style: textStyle,
+    );
+    final textSpan1 = TextSpan(
+      text: '1',
+      style: textStyle,
+    );
+    final textSpan0 = TextSpan(
+      text: '0',
+      style: textStyle,
+    );
+    final textPainter3 = TextPainter(
+      text: textSpan3,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainter2 = TextPainter(
+      text: textSpan2,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainter1 = TextPainter(
+      text: textSpan1,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainter0 = TextPainter(
+      text: textSpan0,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter3.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainter2.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainter1.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainter0.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    final offset3 = Offset(-10, 0);
+    final offset2 = Offset(-10, 30);
+    final offset1 = Offset(-10, 60);
+    final offset0 = Offset(-10, 90);
+    textPainter3.paint(canvas, offset3);
+    textPainter2.paint(canvas, offset2);
+    textPainter1.paint(canvas, offset1);
+    textPainter0.paint(canvas, offset0);
+
+    // WeekDays
+    final textSpanMon = TextSpan(
+      text: 'Mon',
+      style: textStyle,
+    );
+    final textSpanTue = TextSpan(
+      text: 'Tue',
+      style: textStyle,
+    );
+    final textSpanWed = TextSpan(
+      text: 'Wed',
+      style: textStyle,
+    );
+    final textSpanThu = TextSpan(
+      text: 'Thu',
+      style: textStyle,
+    );
+    final textSpanFri = TextSpan(
+      text: 'Fri',
+      style: textStyle,
+    );
+    final textSpanSat = TextSpan(
+      text: 'Sat',
+      style: textStyle,
+    );
+    final textSpanSun = TextSpan(
+      text: 'Sun',
+      style: textStyle,
+    );
+    final textPainterMon = TextPainter(
+      textAlign: TextAlign.center,
+
+      text: textSpanMon,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainterTue = TextPainter(
+      text: textSpanTue,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainterWed = TextPainter(
+      text: textSpanWed,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainterThu = TextPainter(
+      text: textSpanThu,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainterFri = TextPainter(
+      text: textSpanFri,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainterSat = TextPainter(
+      text: textSpanSat,
+      textDirection: TextDirection.ltr,
+    );
+    final textPainterSun = TextPainter(
+      text: textSpanSun,
+      textDirection: TextDirection.ltr,
+    );
+    textPainterMon.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainterTue.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainterWed.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainterThu.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainterFri.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainterSat.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    textPainterSun.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    final offsetMon = Offset(0, 100);
+    final offsetTue = Offset(40, 100);
+    final offsetWed = Offset(80,100);
+    final offsetThu = Offset(130, 100);
+    final offsetFri = Offset(180, 100);
+    final offsetSat = Offset(230, 100);
+    final offsetSun = Offset(280, 100);
+    textPainterMon.paint(canvas, offsetMon);
+    textPainterTue.paint(canvas, offsetTue);
+    textPainterWed.paint(canvas, offsetWed);
+    textPainterThu.paint(canvas, offsetThu);
+    textPainterFri.paint(canvas, offsetFri);
+    textPainterSat.paint(canvas, offsetSat);
+    textPainterSun.paint(canvas, offsetSun);
+
+
+    // Line chart line
     final path = Path()
-      ..moveTo(0, 120)
+      ..moveTo(0, 90)
       ..lineTo(50, 40)
       ..lineTo(100, 85)
       ..lineTo(150, 10)
